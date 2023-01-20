116af77f8c2a517c01bb5b67d06000f25e3c5d21
nikit <nikitaaa6@yandex.ru>
2023-01-20 19:54:19 +0800

    21.11.22

---
+++ src/Author.java
@@ -0,0 +1,13 @@
+public class Author {
+    private  final String firstName, secondName;
+    public Author (String firstName , String secondName ) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+    public String getfirstName(){
+        return firstName;
+    }
+    public String getsecondName(){
+        return secondName;
+    }
+}
